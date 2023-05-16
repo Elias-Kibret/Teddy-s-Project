638351e0e36179dc0a39a7313b20495fb6db6c45
Elias-Kibret <eliaskibret2013n2@gmail.com>
2023-05-16 20:20:40 +0300

    update file

--- script.js
+++ script.js
@@ -1,60 +1,11 @@
-const data = [
-	{
-		title: {
-			value: "Step 1: Eggs",
-		},
-		panel: {
-			value:
-				"Frogs begin life as fertilized eggs. Female frogs lay thousands of eggs at one time in a pond. The eggs float on water in a jelly mass or cluster. Soon, the eggs will hatch into tadpoles!",
-			image: {
-				src: "Frog Lifecycle Step 1.jpg",
-				title:
-					"frog eggs in various stages of development, starting with an olive-colored ball and ending with a visible tail",
-			},
-		},
-	},
-	{
-		title: {
-			value: "Step 2: Tadpoles",
-		},
-		panel: {
-			value:
-				"When a tadpole hatches, it looks more like a fish than a frog. A tadpole doesn\u0026rsquo;t have any legs. It has gills that allow it to breathe underwater. A tadpole swims, eats and grows for several weeks. Nutrients are stored in the tadpole\u0027s tail, as hind legs begin to grow.",
-			image: {
-				src: "Frog Lifecycle Step 2.jpg",
-				title:
-					"tadpoles in various stages of development, ending with an organism with a small frog head with eyes and newly-formed back legs",
-			},
-		},
-	},
-	{
-		title: {
-			value: "Step 3: Froglet",
-		},
-		panel: {
-			value:
-				"As a tadpole grows two front legs, its long tail becomes shorter. The tadpole uses stored nutrients as food, so until its tail is completely gone, it doesn\u0026rsquo;t need anything else to eat! When just a little stub of its tail is left, the tadpole has now become a young frog. It can hop right out of the water and onto dry land for the first time.",
-			image: {
-				src: "Frog Lifecycle Step 3.jpg",
-				title: "3 phases of the froglet\u0027s development",
-			},
-		},
-	},
-	{
-		title: {
-			value: "Step 4: Adult Frog",
-		},
-		panel: {
-			value:
-				"The frog\u0026rsquo;s tail eventually disappears and the frog will begin eating insects instead of plants from the water. A young frog continues to grow for about 2-4 years until it becomes an adult. Adult frogs then lay their own eggs and more tadpoles hatch and begin the cycle again!",
-			image: {
-				src: "Frog Lifecycle Step 4.jpg",
-				title:
-					"two adult frogs: the left frog is younger and smaller, the right frog is a full-grown adult",
-			},
-		},
-	},
-];
+
+//import json files
+
+import data from "./data.json" assert { type: "json" };
+
+
+// Intialiaze DOM 
+
 let container = document.getElementById("main");
 let buttons = document.getElementsByClassName("icon");
 let panel = document.getElementsByClassName("panel");
@@ -62,19 +13,29 @@ let title = document.getElementsByClassName("title");
 let arrowButton = document.getElementsByClassName("arrow");
 let lockButton = document.getElementsByClassName("lock");
 
+
+// Intial state of the accordion
+
 const state = [true, false, false, false];
 let active = false;
+let prevReturned = false;
+let lastChecked = false;
 let intialClicked = false;
 
+
+// Intialiaze DOM
+
 const intial = () => {
-	const AccordionDOM = data?.map((item, index) => {
+
+	//Map for the json data 
+	const AccordionDOM = data?.sections?.map((item, index) => {
 		return `<section>
 				  <div class="header">
 					 <h1 class="title">
 						${item.title.value}
 					 </h1>
 					 <div class="icon">
-            
+                      
 					 <img src="./assets/arrowDown.svg" class="${
 							state[index] ? "show" : "hide"
 						} arrow" alt="arrow down"/>	
@@ -99,21 +60,47 @@ const intial = () => {
 				  </div>
 			</section>`;
 	});
+
+	// put the Mapped json Data into HTML Container 
 	container.innerHTML = AccordionDOM;
+
+	 
+	
 	for (let i = 0; i < buttons.length; i++) {
+
+		//By default all the accordion is closed
+		// Move over the state and turn the default display block to none
+		// Basically we can set this in css file
+
 		if (!state[i] || i == 0) {
 			panel[i].style.display = "none";
 		}
+
+
+		// Added Active class to the first accordion and
+
 		if (i == 0 && state[i]) {
 			title[i].classList.add("active");
 		}
 	}
 };
+
+
+//Run 
 intial();
-let prevReturned;
+
+
+// Add click event to each button
+// Manupulate each button accordingly 
+
 for (let i = 0; i < buttons.length; i++) {
+	//once  the button clicked , activated
 	active = true;
+
+
 	buttons[i].addEventListener("click", () => {
+
+		// Toggle opening and closing of the accordion
 		if (
 			state[i] &&
 			state.slice(i + 1).every((item) => item == false) &&
@@ -125,31 +112,57 @@ for (let i = 0; i < buttons.length; i++) {
 			panel[i].style.display = "flex";
 			state[i] = true;
 		}
+
+		//The first click must be accordion 1;
+		// check is the the accordion click has began 
+
 		if (!intialClicked && i == 0) {
 			panel[i].style.display = "flex";
 			intialClicked = true;
 		}
+        
+		//Toggle the rotation of the arrow icons
 
 		if ((i == 0 || state[i - 1]) && active && i !== state.length - 1) {
+
+			//Add active class to title of the subsequeant elemets
+
 			title[i + 1].classList.add("active");
+
+			//change lock Icon to Arrow icons
 			arrowButton[i + 1].classList.add("show");
 			arrowButton[i + 1].classList.remove("hide");
 			lockButton[i + 1].classList.add("hide");
 			lockButton[i + 1].classList.remove("show");
 		}
-		//Toggle Arrow
+
 		if (state[i]) {
-			console.log("add", i);
+            //Edge case rotation
 			arrowButton[i].classList.add("rotateIcons");
 		}
 		if (!state[i]) {
+			//Edge case rotation
 			arrowButton[i].classList.remove("rotateIcons");
 		}
+
+
+		//Checked if all accordion have been checked 
+		
 		const currentReturn = state.every((item) => item == false);
 
-		if (intialClicked && state[0] && prevReturned) {
-			intial();
+		//Resturn to the initial value
+
+		if (intialClicked && state[0] && prevReturned && lastChecked) {
+            //reload
+			intialClicked = false
+			prevReturned=false
+			state[0]
+			location.reload()
 		}
-		prevReturned = currentReturn;
+		//Check is the last accordion has checked
+		if (state[state.length - 1]) {
+			lastChecked = true;
+		}
+		
 	});
 }
